76f9a3524e5c76b2cf80c43c7bcff72ae5523f31
michu <michu.ad@wp.pl>
2025-01-20 22:10:27 +0100

    Dodaj pliki projektów.

---
+++ Otodom/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Otodom
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Otodom/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Otodom
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
