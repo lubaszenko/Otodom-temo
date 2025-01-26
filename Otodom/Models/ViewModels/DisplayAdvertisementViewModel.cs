765a014b7004ed414016f680b4376836d6153bd5
michu <michu.ad@wp.pl>
2025-01-26 20:29:18 +0100

    Dodawanie funkcji usuwania ogłoszenia.

--- Otodom/Models/ViewModels/DisplayAdvertisementViewModel.cs
+++ Otodom/Models/ViewModels/DisplayAdvertisementViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using ApiConsumer;
 using System.Collections.ObjectModel;
 using ApiConsumer.DTO;
+using System.ComponentModel;
 
 namespace Otodom.Models.ViewModels
 {
@@ -11,11 +12,15 @@ namespace Otodom.Models.ViewModels
     {
         private readonly ApiClientService _clientService;
 
+        public ICommand DeleteCommand { get; }
+
         public ICommand LoadDataHouseCommand { get; }
 
         public ICommand NavigateBackCommand { get; }
 
         public ICommand ShowDetailsCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         [ObservableProperty]
         private ObservableCollection<OgloszenieResponse> _ogloszenieznieruchomoscias = new();
@@ -23,10 +28,25 @@ namespace Otodom.Models.ViewModels
         public DisplayAdvertisementViewModel(ApiClientService clientService) 
         {
             _clientService = clientService;
+            DeleteCommand = new Command<int>(async (id) => await DeleteOgloszenie(id));
             ShowDetailsCommand = new Command<OgloszenieResponse>(ShowDetails);
             NavigateBackCommand = new AsyncRelayCommand(NavigateBackCommandAsync);
             LoadDataHouseCommand = new AsyncRelayCommand(LoadDataHouseAsync);
             LoadDataHouseCommand.Execute(null);
+        }
+
+        private async Task DeleteOgloszenie(int id)
+        {
+            var success = await _clientService.DeleteOgloszenieAsync(id);
+            if (success)
+            {
+                await LoadDataHouseAsync();
+                await Application.Current.MainPage.DisplayAlert("Sukces", "Ogłoszenie usunięte.", "OK");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Błąd", "Nie udało się usunąć ogłoszenia.", "OK");
+            }
         }
 
         private async Task LoadDataHouseAsync()
@@ -35,7 +55,8 @@ namespace Otodom.Models.ViewModels
             {
                 Console.WriteLine("Endpoint LoadDataHouse works.");
                 List<OgloszenieResponse> ogloszenies = await _clientService.GetOgloszenieznieruchomoscia();
-                foreach(var Ogloszenie in ogloszenies)
+                _ogloszenieznieruchomoscias.Clear();
+                foreach (var Ogloszenie in ogloszenies)
                 {
                     if(!_ogloszenieznieruchomoscias.Any(o => o.Id == Ogloszenie.Id))
                     {
@@ -56,7 +77,7 @@ namespace Otodom.Models.ViewModels
 
             string szczegoly = $"Imię wystawiającego: {ogloszenie.ImieKlienta}\n" +
                                $"Telefon: {ogloszenie.Telefon}\n" +
-                               $"E-mail: { ogloszenie.Email}";
+                               $"E-mail: {ogloszenie.Email}";
 
             await Application.Current.MainPage.DisplayAlert("Dane kontaktowe", szczegoly, "OK");
         }
